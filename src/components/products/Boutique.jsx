88b68701682a265ef08131b1b6aa6cbc6b151672
Yugo <yugo7@protonmail.com>
2024-10-18 17:15:31 +0100

    Bugfixes

--- src/components/products/Boutique.jsx
+++ src/components/products/Boutique.jsx
@@ -1,25 +1,30 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import SidebarWithHeader from "../shared/SideBar.jsx";
 import OurPicks from "./OurPicks.jsx";
-import {Button, Image, Spinner, Stack, Text, useBreakpointValue} from "@chakra-ui/react";
+import { Button, Image, Spinner, Stack, Text, useBreakpointValue } from "@chakra-ui/react";
 import ProductList from "./ProductList.jsx";
-import {getProductsBySection, Sections} from "../../services/productsService.jsx";
+import { getProductsBySection, Sections } from "../../services/productsService.jsx";
 import { useTranslation } from 'react-i18next';
 
 export default function Boutique() {
 
-    const fontHeadlineSize = useBreakpointValue({base: "lg", md: "2xl"});
+    const fontHeadlineSize = useBreakpointValue({ base: "lg", md: "2xl" });
     const [isLoading, setIsLoading] = useState(false);
     const [products, setProducts] = useState([]);
     const [section, setSection] = useState();
-    const fontSize = useBreakpointValue({base: "5xl", md: "62px"});
+    const fontSize = useBreakpointValue({ base: "5xl", md: "62px" });
     const { t } = useTranslation();
 
     useEffect(() => {
         setIsLoading(true);
         getProductsBySection(section)
             .then(data => {
-                setProducts(data);
+                const sortedProducts = data.sort((a, b) => {
+                    if (a.secao > b.secao) return -1;
+                    if (a.secao < b.secao) return 1;
+                    return 0;
+                });
+                setProducts(sortedProducts);
                 setIsLoading(false);
             })
             .catch(error => {
@@ -31,17 +36,17 @@ export default function Boutique() {
     return (
         <SidebarWithHeader>
             <Stack justify="flex-start" align="center" my={6} mx={4} spacing="24px">
-                <Text className="cafelab" align="center" fontSize={fontSize}  color="#000000">
-                    {t('boutique.title')}
+                <Text className="cafelab" align="center" fontSize={fontSize} color="#000000">
+                    {t('boutique.title').toUpperCase()}
                 </Text>
                 <Text maxW={"800px"} fontFamily="Roboto" fontWeight="regular" fontSize={fontHeadlineSize} letterSpacing="tighter" color="black"
-                      textAlign="center" mx={4}>
+                    textAlign="center" mx={4}>
                     {t('boutique.description')}
                 </Text>
             </Stack>
 
             <Stack className={"main-panel"} Width="100wv"
-                   >
+            >
                 <Image
                     alignSelf="center"
                     src='assets/capa_boutique.jpeg'
@@ -56,21 +61,21 @@ export default function Boutique() {
                             setSection(prevSection => prevSection === Sections.CAFE ? null : Sections.CAFE);
                             event.currentTarget.blur();
                         }}>
-                            {t('boutique.coffeeButton')}
+                            {t('boutique.coffeeButton').toUpperCase()}
                         </Button>
                         <Button variant={"solid"} backgroundColor={"blackAlpha.800"} color={"antiquewhite"} onClick={(event) => {
                             setSection(prevSection => prevSection === Sections.BOUTIQUE ? null : Sections.BOUTIQUE);
                             event.currentTarget.blur();
                         }}>
-                            {t('boutique.boutiqueButton')}
+                            {t('boutique.boutiqueButton').toUpperCase()}
                         </Button>
                     </Stack>
                 </Stack>
 
                 {isLoading ? (
-                    <Spinner/>
+                    <Spinner />
                 ) : (
-                    <ProductList products={products}/>
+                    <ProductList products={products} />
                 )}
 
                 <Stack align={"center"} mx={10} p={10}>
