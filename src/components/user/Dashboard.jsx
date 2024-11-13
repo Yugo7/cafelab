df110548b126f39dd39627cdadc3ccdcaffb788a
Yugo <yugo7@protonmail.com>
2024-11-13 01:27:59 +0000

    Dashboard impovements

--- src/components/user/Dashboard.jsx
+++ src/components/user/Dashboard.jsx
@@ -3,18 +3,9 @@ import {
     Avatar,
     Box,
     Button,
-    HStack,
-    Modal,
-    ModalBody,
-    ModalCloseButton,
-    ModalContent,
-    ModalFooter,
-    ModalHeader,
-    ModalOverlay,
     Spinner,
     Stack,
     Text,
-    Textarea,
     useDisclosure,
     useToast,
     VStack
@@ -23,14 +14,11 @@ import SidebarWithHeader from "../shared/SideBar.jsx";
 import { useAuth } from "../context/AuthContext.jsx";
 import OrderService from "../../services/orderService.jsx";
 import { useNavigate } from "react-router-dom";
-import { formatCurrency } from "../utilities/formatCurrency.jsx";
 import { useShoppingCart } from "../context/ShoppingCartContext.jsx";
-import { Image } from "react-bootstrap";
-import axios from "axios";
 import { useTranslation } from "react-i18next";
 import MyOrders from "./MyOrders.jsx";
+import MySubscriptions from "./MySubscriptions.jsx";
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 const ProfilePage = () => {
     const { customer, resetPassword } = useAuth();
     const [orders, setOrders] = useState([]);
@@ -43,29 +31,6 @@ const ProfilePage = () => {
     const [submitting, setSubmitting] = useState(false);
     const { t } = useTranslation();
 
-    const sendContactMessage = (message) => {
-        setSubmitting(true);
-        const values = {
-            description: message,
-            name: customer.id,
-            email: customer.username,
-        }
-        axios.post(`${BASE_URL}contacts/`, values)
-            .then(() => {
-                setSubmitting(false);
-                toast({
-                    title: t('contactForm.successTitle'),
-                    description: t('contactForm.successDescription'),
-                    status: "success",
-                    duration: 9000,
-                    isClosable: true,
-                });
-                onClose()
-            })
-            .catch(() => {
-                setSubmitting(false);
-            });
-    }
 
     const partition = (array, isValid) => {
         return [
@@ -77,10 +42,6 @@ const ProfilePage = () => {
     const handleChangePassword = () => {
         resetPassword(customer.username);
     };
-
-    const cancel = (id) => {
-        OrderService.cancelSubscription(id)
-    }
 
     useEffect(() => {
         const fetchOrders = async () => {
@@ -109,7 +70,7 @@ const ProfilePage = () => {
         <SidebarWithHeader>
             <Box w="100%" justifyContent={"center"}>
                 <VStack justifyContent="center" textAlign={"center"} alignItems="center" w="100vw" p={5}>
-                    <Box borderRadius="lg" overflow="hidden" w={"100vw"} maxW={"800px"} p={5} mt={5}>
+                    <Box borderRadius="lg" overflow="hidden" w={"100vw"} p={5} mt={5}>
                         <Avatar
                             name={customer.name}
                             size={'xl'}
@@ -122,94 +83,25 @@ const ProfilePage = () => {
                     </Box>
                 </VStack>
 
-                <VStack justifyContent="center" alignItems="center" w="100vw" p={5}>
+                <Stack justifyContent="center" alignItems="center" w="100vw" p={5}>
                     <Box borderWidth="1px" borderRadius="lg" p={4} w={"100vw"} maxW={"800px"}>
-
                         <Stack width={"100%"}>
                             <Text className="cafelab" fontWeight={"medium"} fontSize={"5xl"} align={"center"} mb={4}>
                                 {t('userDashboard.yourSubscription')}
                             </Text>
-                            { subscriptions.length > 0 ? subscriptions.map((subscription, index) => (
-                                    <>
-                                        <HStack margin={"auto"} justifyContent="space-evenly">
-                                            <Stack gap={2} className="d-flex align-items-center" maxW={"150px"}>
-                                                <Text className="cafelab" fontWeight={"medium"} fontSize={"xl"} align={"center"} mb={4}>
-                                                    {subscription.products.name.toUpperCase()}
-                                                </Text>
-                                                <Image
-                                                    src={subscription.products.image}
-                                                    style={{ width: "120px", objectFit: "cover" }}
-                                                />
-                                            </Stack>
-                                            <Stack>
-                                                <Text className="cafelab" fontWeight={"medium"} fontSize={"xl"} align={"center"} mb={4}>
-                                                    {subscription.products.periodicity_string.toUpperCase()}
-                                                </Text>
-                                                <Stack className="d-flex align-items-left">
-                                                    {subscription.products.coffee ? subscription.products.coffee.map((coffee) => (
-                                                        <>
-                                                            <Stack direction={"row"}>
-                                                                <Text className="cafelab" fontWeight={"normal"} fontSize={"lg"} align={"left"}>
-                                                                    - {coffee.name.toUpperCase()} x {coffee.quantity}
-                                                                </Text>
-                                                            </Stack>
-                                                            <Modal isOpen={isOpen} onClose={onClose} size={"md"}>
-                                                                <ModalOverlay />
-                                                                <ModalContent>
-                                                                    <ModalHeader>{t('userDashboard.changeCoffees')}</ModalHeader>
-                                                                    <ModalCloseButton />
-                                                                    <ModalBody>
-                                                                        <Text>{t('userDashboard.indicateCoffees')}</Text>
-                                                                        <Textarea
-                                                                            mt={6}
-                                                                            value={message}
-                                                                            onChange={(e) => setMessage(e.target.value)}
-                                                                            placeholder={t('userDashboard.writeMessage')}
-                                                                        />
-                                                                    </ModalBody>
-                                                                    <ModalFooter>
-                                                                        <Button onClick={() => sendContactMessage(message)}>
-                                                                            {t('userDashboard.send')}
-                                                                        </Button>
-                                                                    </ModalFooter>
-                                                                </ModalContent>
-                                                            </Modal>
-                                                        </>
-                                                    )) : null}
-                                                    {subscription.products.coffee ? (
-                                                        <Button onClick={onOpen}>
-                                                            {t('userDashboard.changeCoffees')}
-                                                        </Button>) : null}
-
-                                                    <Text className="ms-auto" pt={6} fontSize={"lg"}>
-                                                            {formatCurrency(
-                                                                subscription.total
-                                                            )} {t('userDashboard.every')} {subscription.products.periodicity} {t('userDashboard.months')}
-                                                    </Text>
-                                                        <Button variant={"outline"} colorScheme={"red"} onClick={cancel(subscription.id)}>
-                                                        {t('userDashboard.cancelSubscription')}
-                                                        </Button>
-                                                    </Stack>
-                                            </Stack>
-                                        </HStack>
-                                    </>
-                                )) : (
-                                    <Stack my={8} textAlign={"center"}><Text>{t('userDashboard.noSubscription')}</Text></Stack>
-                                )
-                            }
                         </Stack>
+                        <MySubscriptions subscriptions={subscriptions} />
                     </Box>
-                </VStack>
-                <VStack justifyContent="center" alignItems="center" w="100vw" p={5}>
+                </Stack>
+                <Stack justifyContent="center" alignItems="center" w="100vw" p={5}>
                     <Box borderWidth="1px" borderRadius="lg" p={4} w={"100vw"} maxW={"800px"}>
                         <Text className="cafelab" fontWeight={"medium"} fontSize={"5xl"} align={"center"} mb={4}>
                             {t('userDashboard.myOrders').toUpperCase()}
                         </Text>
                         <MyOrders>
-
                         </MyOrders>
                     </Box>
-                </VStack>
+                </Stack>
 
             </Box>
         </SidebarWithHeader>
---
+++ src/components/user/MySubscriptions.jsx
@@ -0,0 +1,145 @@
+import {
+    Button,
+    Modal,
+    ModalBody,
+    ModalCloseButton,
+    ModalContent,
+    ModalFooter,
+    ModalHeader,
+    ModalOverlay, Stack, Tag,
+    Text,
+    Textarea, useToast
+} from "@chakra-ui/react";
+import {formatCurrency} from "@/components/utilities/formatCurrency.jsx";
+import React, {useState} from "react";
+import {useTranslation} from "react-i18next";
+import axios from "axios";
+import {useAuth} from "@/components/context/AuthContext.jsx";
+import OrderService from "@/services/orderService.jsx";
+
+const MySubscriptions = ({subscriptions}) => {
+    const {t} = useTranslation();
+    const [isOpen, setIsOpen] = useState(false);
+    const [message, setMessage] = useState("");
+    const [submitting, setSubmitting] = useState(false);
+    const toast = useToast();
+    const { customer, } = useAuth();
+
+    const onOpen = () => setIsOpen(true);
+    const onClose = () => setIsOpen(false);
+
+
+    const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+    const sendContactMessage = (message) => {
+        setSubmitting(true);
+        const values = {
+            description: message,
+            name: customer.id,
+            email: customer.username,
+        }
+        axios.post(`${BASE_URL}contacts/`, values)
+            .then(() => {
+                setSubmitting(false);
+                toast({
+                    title: t('contactForm.successTitle'),
+                    description: t('contactForm.successDescription'),
+                    status: "success",
+                    duration: 9000,
+                    isClosable: true,
+                });
+                onClose()
+            })
+            .catch(() => {
+                setSubmitting(false);
+            });
+    }
+
+    const cancel = (id) => {
+        OrderService.cancelSubscription(id)
+    }
+
+    return (
+        <>
+            {subscriptions.length > 0 ? subscriptions.map((subscription, index) => (
+
+                <Stack direction={{ base: "column", md: 'row' }} key={index} justifyContent="space-evenly" my={6}
+                        borderBottom="1px solid #cbd5e0" pb={4} alignItems="center">
+                    <Stack gap={2} className="d-flex align-items-center" textAlign={"center"} w={{ base: "50%", md: '100%' }} >
+                        <Text className="cafelab" fontWeight={"medium"} fontSize={"xl"} align={"center"} >
+                            {subscription.products.name.toUpperCase()}
+                        </Text>
+                        <Text className="cafelab" fontWeight={"medium"} fontSize={"xl"} align={"center"} >
+                            {subscription.products.periodicity_string.toUpperCase()}
+                        </Text>
+                        <Text className="cafelab" fontWeight={"medium"}  fontSize={"lg"}>
+                            {t('userDashboard.sinceDate')}:
+                            <br/> {new Date(subscription.created_at).toLocaleDateString('en-GB')}
+                        </Text>
+                        <Tag colorScheme={subscription.status === 'ACTIVE' ? 'green' : subscription.status === 'PENDING' ? 'yellow' : 'red'}>
+                            {subscription.status === 'ACTIVE' ? t('myOrders.status.active').toUpperCase() : subscription.status === 'PENDING' ? t('myOrders.status.pending').toUpperCase() : t('myOrders.status.inactive').toUpperCase() }
+                        </Tag>
+                        {subscription.status === 'ACTIVE' ? (
+                            <Text className="cafelab" fontWeight={"medium"}  fontSize={"lg"}>
+                                {t('userDashboard.nextShipping')}:
+                                <br/> 02/{new Date().getMonth() + 2}/{new Date().getFullYear()}
+                            </Text>
+                        )  : (
+                            <Text className="cafelab" pt={6} fontWeight={"medium"}  fontSize={"lg"}>
+                                {t('userDashboard.pendencies')}
+                            </Text>
+                        )}
+                    </Stack>
+                    <Stack pt={8} className="d-flex align-items-center" textAlign={"center"} w={{ base: "50%", md: '100%' }} >
+                        <Stack>
+                            {subscription.products.coffee ? subscription.products.coffee.map((coffee, coffeeIndex) => (
+                                <Stack key={coffeeIndex} direction={"row"}>
+                                    <Text className="cafelab" fontWeight={"normal"} fontSize={"lg"} align={"left"}>
+                                        - {coffee.name.toUpperCase()} x {coffee.quantity}
+                                    </Text>
+                                </Stack>
+                            )) : null}
+                            {subscription.products.coffee ? (
+                                <Button onClick={onOpen}>
+                                    {t('userDashboard.changeCoffees')}
+                                </Button>
+                            ) : null}
+
+                            <Text className="ms-auto" pt={6} fontSize={"lg"}>
+                                {formatCurrency(subscription.total)} {t('userDashboard.every')} {subscription.products.periodicity} {t('userDashboard.months')}
+                            </Text>
+                            <Button variant={"outline"} colorScheme={"red"} onClick={() => cancel(subscription.id)}>
+                                {t('userDashboard.cancelSubscription')}
+                            </Button>
+                        </Stack>
+                    </Stack>
+                </Stack>
+            )) : (
+                <Stack my={8} textAlign={"center"}><Text>{t('userDashboard.noSubscription')}</Text></Stack>
+            )}
+
+            <Modal isOpen={isOpen} onClose={onClose} size={"md"}>
+                <ModalOverlay/>
+                <ModalContent>
+                    <ModalHeader>{t('userDashboard.changeCoffees')}</ModalHeader>
+                    <ModalCloseButton/>
+                    <ModalBody>
+                        <Text>{t('userDashboard.indicateCoffees')}</Text>
+                        <Textarea
+                            mt={6}
+                            value={message}
+                            onChange={(e) => setMessage(e.target.value)}
+                            placeholder={t('userDashboard.writeMessage')}
+                        />
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button onClick={() => sendContactMessage(message)}>
+                            {t('userDashboard.send')}
+                        </Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+        </>
+    );
+}
+
+export default MySubscriptions;
